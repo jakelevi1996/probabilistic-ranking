43a1ed7550c80e285915cb4aee5df83c15727fcf
jakelevi1996 <jakelevi@hotmail.co.uk>
2018-11-11 21:50:04 +0000

    Calculate skill marginals for EP ranking

--- Code/ranking.py
+++ Code/ranking.py
@@ -1,16 +1,17 @@
 import numpy as np
 from scipy.linalg import cholesky, solve_triangular as solve
+from scipy.stats import norm
 from time import time
 
 def gibbs_rank(games, num_players, num_steps=1100, print_every=None):
-    if print_every is None: print_every = int(num_steps / 5)
+    if print_every is None: print_every = min(100, int(num_steps / 5))
     assert games.max() < num_players
     num_games = games.shape[0]
     # Initialise skills to prior mean (IE zero, by symmetry)
     skills = np.zeros([num_players, 1])
     skills_history = np.zeros([num_players, num_steps])
-    # Initialise prior precision of skills
-    prior_skills_precision = 2 * np.identity(num_players)
+    # Initialise prior variance of skills
+    prior_skills_var = 0.5
     for step in range(num_steps):
         if step % print_every == 0: print("Step = {}".format(step))
         # Sample performance differences per game, given skills and outcomes:
@@ -25,7 +26,7 @@ def gibbs_rank(games, num_players, num_steps=1100, print_every=None):
             )
             reject_inds = d_perf < 0
         # Initialise posterior precision of skills to prior
-        post_skills_precision = 1.0 * prior_skills_precision
+        post_skills_precision = np.identity(num_players) / prior_skills_var
         # Update diagonal terms of posterior precision of skills
         players, matches_per_player = np.unique(games, return_counts=True)
         post_skills_precision[players, players] += matches_per_player
@@ -33,8 +34,8 @@ def gibbs_rank(games, num_players, num_steps=1100, print_every=None):
         pairs, matches_per_pair = np.unique(games, return_counts=True, axis=0)
         post_skills_precision[pairs[:, 0], pairs[:, 1]] -= matches_per_pair
         post_skills_precision[pairs[:, 1], pairs[:, 0]] -= matches_per_pair
-        # Calculate the product of the posterior skills precision and mean
-        post_skills_precision_mean = (np.where(
+        # Calculate the posterior skills 'natural mean' (precision * mean)
+        post_skills_natural_mean = (np.where(
             games[:, 0].reshape(-1, 1) == np.arange(num_players), d_perf, 0
         ) - np.where(
             games[:, 1].reshape(-1, 1) == np.arange(num_players), d_perf, 0
@@ -44,7 +45,7 @@ def gibbs_rank(games, num_players, num_steps=1100, print_every=None):
         # Sample skills per player, given performance differences:
         skills = solve(
             L, solve(
-                L.T, post_skills_precision_mean,
+                L.T, post_skills_natural_mean,
                 lower=True, check_finite=False
             ) + np.random.normal(size=[num_players, 1]),
             lower=False, check_finite=False
@@ -56,8 +57,38 @@ def gibbs_rank(games, num_players, num_steps=1100, print_every=None):
     std_skills = skills_history.std(axis=1)
     return skills_history, mean_skills, std_skills
 
+def psi_func(x):
+    return norm.pdf(x) / norm.cdf(x)
+
+def lambda_func(x):
+    return psi_func(x) * (psi_func(x) + x)
+
+def ep_rank(games, num_players, num_steps=5):
+    assert games.max() < num_players
+    num_games = games.shape[0]
+    # Initialise skills to prior mean (IE zero, by symmetry)
+    skills = np.zeros([num_players, 1])
+    skills_history = np.zeros([num_players, num_steps])
+    # Initialise prior variance of skills
+    prior_skills_variance = 0.5
+    # Initialise game to skill messages
+    game_to_skill_means = np.zeros([num_games, 2])
+    game_to_skill_precs = np.zeros([num_games, 2])
+    for step in range(num_steps):
+        print("Step = {}".format(step))
+        # Step 1: compute marginal skills:
+        skill_precs = 1.0 / prior_skills_variance + (np.where(
+            games.reshape(-1, 1) == np.arange(num_players),
+            game_to_skill_precs.reshape(-1, 1), 0
+        )).sum(axis=0).reshape(-1, 1)
+        skill_means = 1.0 / skill_precs * (np.where(
+            games.reshape(-1, 1) == np.arange(num_players),
+            (game_to_skill_precs * game_to_skill_means).reshape(-1, 1), 0
+        )).sum(axis=0).reshape(-1, 1)
+
+
 if __name__ == "__main__":
-    # np.random.seed(27)
+    np.random.seed(27)
     
     # games = np.array([
     #     [0, 1],
@@ -75,11 +106,11 @@ if __name__ == "__main__":
         [0, 1],
         [1, 2],
         [2, 3],
-        [2, 3],
-        [2, 3],
-        [2, 3],
-        [2, 3],
-        [2, 3],
+        # [2, 3],
+        # [2, 3],
+        # [2, 3],
+        # [2, 3],
+        # [2, 3],
         [3, 4],
         [4, 5],
         [0, 2],
@@ -101,5 +132,6 @@ if __name__ == "__main__":
         num_steps, time() - start_time
     ))
     print(np.concatenate(
-        [mean_skills.reshape(-1, 1), std_skills.reshape(-1, 1)], axis=1)
-    )
+        [mean_skills.reshape(-1, 1), std_skills.reshape(-1, 1)], axis=1
+    ))
+    ep_rank(games, num_players, num_steps=5)
