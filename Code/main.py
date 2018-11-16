48e1bf9a6b77bea67ee83441bfe80a0e728beb35
jakelevi1996 <jakelevi@hotmail.co.uk>
2018-11-16 01:46:00 +0000

    Plot traces for custom lists of players

--- Code/main.py
+++ Code/main.py
@@ -4,23 +4,25 @@ from time import time
 import plotting, ranking
 from data.fileio import load
 
-def question_a(games, names, num_players, num_steps=2000):
-    # TODO: change `num_players` to `players` list
+def question_a(games, names, num_players, players, num_steps=2000):
     skill_history, skill_means, skill_stds = ranking.gibbs_rank(
         games, num_players, num_steps
     )
-    plotting.plot_gibbs_skills(skill_history, skill_means, names)
+    plotting.plot_gibbs_skills(skill_history, skill_means, names, players)
     plotting.plot_gibbs_correlations(
-        skill_history, skill_means, names, maxlags=15
+        skill_history, skill_means, names, players, maxlags=100
     )
     return skill_history, skill_means, skill_stds
 
-def question_b(games, names, num_players, num_steps=81):
+def question_b(games, names, num_players, players, num_steps=81):
     ep_skill_means, ep_skill_stds = ranking.ep_rank(
         games, num_players, num_steps
     )
     plotting.plot_ep_skills(
-        ep_skill_means, ep_skill_stds, names, 6, plot_std=True
+        ep_skill_means, ep_skill_stds, names, players, plot_std=True
+    )
+    plotting.plot_ep_skills_cyclic_colours(
+        ep_skill_means, ep_skill_stds, names, range(num_players),
     )
     return ep_skill_means, ep_skill_stds
 
@@ -55,10 +57,10 @@ def question_e(
         games, num_players
     )
     gibbs_sort = ranking.skill_rank(gibbs_skill_means)
-    plotting.plot_rank_bars(
-        empirical_skills, gibbs_skill_means, ep_skill_means,
-        gibbs_sort, names, num_players
-    )
+    # plotting.plot_rank_bars(
+    #     empirical_skills, gibbs_skill_means, ep_skill_means,
+    #     gibbs_sort, names, num_players
+    # )
     plotting.plot_rank_lines(
         empirical_skills, gibbs_skill_means, ep_skill_means,
         gibbs_sort, names, num_players
@@ -84,11 +86,11 @@ if __name__ == "__main__":
     games, names = load()
     num_players = names.size
     # save_example_skills_history()
-    gibbs_skill_samples, gibbs_skill_means, gibbs_skill_stds = question_a(
-        games, names, num_players, num_steps=2000
-    )
+    # gibbs_skill_samples, gibbs_skill_means, gibbs_skill_stds = question_a(
+    #     games, names, num_players, top_4_players, num_steps=2000
+    # )
     ep_skill_means_history, ep_skill_stds_history = question_b(
-        games, names, num_players, num_steps=100
+        games, names, num_players, top_4_players, num_steps=100
     )
     ep_skill_means = ep_skill_means_history[:, -1]
     ep_skill_stds = ep_skill_stds_history[:, -1]
@@ -96,12 +98,12 @@ if __name__ == "__main__":
         ep_skill_means, ep_skill_stds,
         names, top_4_players
     )
-    question_d(
-        gibbs_skill_samples, gibbs_skill_means, gibbs_skill_stds,
-        names, [0, 15]
-    )
-    question_d(
-        gibbs_skill_samples, gibbs_skill_means, gibbs_skill_stds,
-        names, top_4_players
-    )
-    question_e(games, names, num_players, gibbs_skill_means, ep_skill_means)
+    # question_d(
+    #     gibbs_skill_samples, gibbs_skill_means, gibbs_skill_stds,
+    #     names, [0, 15]
+    # )
+    # question_d(
+    #     gibbs_skill_samples, gibbs_skill_means, gibbs_skill_stds,
+    #     names, top_4_players
+    # )
+    # question_e(games, names, num_players, gibbs_skill_means, ep_skill_means)
--- Code/plotting.py
+++ Code/plotting.py
@@ -5,20 +5,23 @@ from data.fileio import load
 DEFAULT_FILENAME = "Code/results/img"
 
 def plot_gibbs_skills(
-    skills_history, mean_skills, names, num_players=5,
+    skills_history, mean_skills, names, players,
     filename=DEFAULT_FILENAME, figsize=[16, 9], xlim=None, fmt=""
 ):
     plt.figure(figsize=figsize)
-    for player in range(num_players):
-        plt.plot(skills_history[player], fmt, alpha=0.7)
+    # Plot traces:
+    for p in players:
+        plt.plot(skills_history[p], fmt, alpha=0.7)
+    # Reset colour cycle and plot means:
     plt.gca().set_prop_cycle(None)
-    for player in range(num_players):
+    for p in players:
         plt.plot(
             [0, skills_history.shape[1]],
-            [mean_skills[player], mean_skills[player]]
+            [mean_skills[p], mean_skills[p]]
         )
+    # Format and save:
     plt.grid(True)
-    plt.legend(["{} ({})".format(names[i], i+1) for i in range(num_players)])
+    plt.legend(["{} ({})".format(names[p], p+1) for p in players])
     plt.xlabel("Gibbs iteration")
     plt.ylabel("Skill")
     plt.title("Tennis player skills found using Gibbs ranking")
@@ -27,20 +30,20 @@ def plot_gibbs_skills(
     plt.close()
 
 def plot_gibbs_correlations(
-    skills_history, mean_skills, names, num_players=5, maxlags=100,
+    skills_history, mean_skills, names, players, maxlags=100,
     filename=DEFAULT_FILENAME, figsize=None
 ):
     if figsize is not None: plt.figure(figsize=figsize)
     else: plt.figure()
     
-    for player in range(num_players):
-        trace = skills_history[player] - mean_skills[player]
+    for p in players:
+        trace = skills_history[p] - mean_skills[p]
         plt.xcorr(
             trace, trace, maxlags=maxlags, usevlines=False, linestyle="-",
             marker=None, alpha=0.7
         )
     plt.grid(True)
-    plt.legend(["{} ({})".format(names[i], i+1) for i in range(num_players)])
+    plt.legend(["{} ({})".format(names[p], p+1) for p in players])
     plt.xlabel("Lag")
     plt.ylabel("Auto-correlation")
     plt.title("Auto-correlations for skill traces found using Gibbs ranking")
@@ -48,23 +51,23 @@ def plot_gibbs_correlations(
     plt.close()
 
 def plot_ep_skills(
-    skill_means_history, skill_stds_history, names, num_players=5,
+    skill_means_history, skill_stds_history, names, players,
     filename=DEFAULT_FILENAME, figsize=[16, 9], plot_std=True
 ):
     plt.figure(figsize=figsize)
-    for player in range(num_players):
-        plt.plot(skill_means_history[player])
+    for p in players:
+        plt.plot(skill_means_history[p])
     plt.gca().set_prop_cycle(None)
     if plot_std:
-        for player in range(num_players):
+        for p in players:
             plt.fill_between(
                 range(skill_means_history.shape[1]),
-                (skill_means_history[player] + 2.0*skill_stds_history[player]),
-                (skill_means_history[player] - 2.0*skill_stds_history[player]),
+                (skill_means_history[p] + 2.0*skill_stds_history[p]),
+                (skill_means_history[p] - 2.0*skill_stds_history[p]),
                 alpha=0.2
             )
     plt.grid(True)
-    plt.legend(["{} ({})".format(names[i], i+1) for i in range(num_players)])
+    plt.legend(["{} ({})".format(names[p], p+1) for p in players])
     plt.xlabel("EP iteration")
     plt.ylabel("Skill")
     plt.title("Tennis player skills found using EP ranking")
@@ -72,16 +75,17 @@ def plot_ep_skills(
     plt.close()
 
 def plot_ep_skills_cyclic_colours(
-    skill_means_history, skill_stds_history, names, num_players=5,
-    filename=DEFAULT_FILENAME, figsize=[16, 9],
+    skill_means_history, skill_stds_history, names, players,
+    filename=DEFAULT_FILENAME, figsize=[16, 9], alpha=0.3
 ):
     plt.figure(figsize=figsize)
-    # colours = iter(plt.cm.get_cmap("viridis")
-    # colours = iter(plt.cm.bwr(np.linspace(0, 1, num_players)))
-    # for player in range(num_players):
-    #     plt.plot(skill_means_history[player], c=next(colours))
+    # colours = iter(plt.cm.get_cmap("viridis"))
+    # colours = iter(plt.cm.bwr(np.linspace(0, 1, len(players))))
+    for p in players:
+        # plt.plot(skill_means_history[p], c=next(colours), alpha=alpha)
+        plt.plot(skill_means_history[p], 'b', alpha=alpha)
     plt.grid(True)
-    plt.legend(["{} ({})".format(names[i], i+1) for i in range(num_players)])
+    # plt.legend(["{} ({})".format(names[p], p+1) for p in players])
     plt.xlabel("EP iteration")
     plt.ylabel("Skill")
     plt.title("Tennis player skills found using EP ranking")
@@ -106,7 +110,7 @@ def plot_rank_bars(
     plt.gca().xaxis.grid(True)
     plt.xlabel("Normalised skill")
     plt.ylabel("Player name")
-    plt.title("Normalised skills for each player")
+    plt.title("Normalised skills for each player, ordered by Gibbs rank")
     plt.legend(["Empirical skills", "Gibbs skills", "EP skills"])
     plt.yticks(fontsize=5)
     plt.savefig(filename, dpi=200)
@@ -114,13 +118,16 @@ def plot_rank_bars(
 
 def plot_rank_lines(
     empirical_skills, gibbs_skill_means, ep_skill_means, gibbs_sort,
-    names, num_players, filename=DEFAULT_FILENAME, figsize=[16, 9]
+    names, num_players, filename=DEFAULT_FILENAME, figsize=[16, 9], alpha=0.8
 ):
     plt.figure(figsize=figsize)
     for skill in [empirical_skills, gibbs_skill_means, ep_skill_means]:
-        plt.plot(range(num_players, 0, -1), scale_array(skill[gibbs_sort]))
+        plt.plot(
+            range(num_players, 0, -1), scale_array(skill[gibbs_sort]),
+            alpha=alpha
+        )
     plt.grid(True)
-    plt.xlabel("Player rank (EP)")
+    plt.xlabel("Player rank (Gibbs)")
     plt.ylabel("Normalised skill")
     plt.title("Normalised skills for each player as a function of Gibbs rank")
     plt.legend(["Empirical skills", "Gibbs skills", "EP skills"])
@@ -136,8 +143,7 @@ if __name__ == "__main__":
     # fmt = "o-"
     # xlim = 200
     plot_gibbs_skills(
-        skills_history, mean_skills, names, xlim=xlim, fmt=fmt, num_players=10
+        skills_history, mean_skills, names, range(5), xlim=xlim, fmt=fmt
     )
     # plot_gibbs_correlations(skills_history, mean_skills, names, maxlags=15)
     print(scale_array(np.array([3, 6, 7])))
-
